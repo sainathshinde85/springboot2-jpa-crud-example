ab5638cb0849aae90682927b53b47a83cf9d50f1
saishinde <sainathshinde85@gmail.com>
2022-08-10 19:08:36 +0530

    Student class added and sop added into Application.java

---
+++ src/main/java/net/guides/springboot2/springboot2jpacrudexample/model/Student.java
@@ -0,0 +1,8 @@
+package net.guides.springboot2.springboot2jpacrudexample.model;
+
+public class Student {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String email;
+}
